cf7a220f163e6362de778a6b8cfccae1b3f3c262
insukChoi <cis1725@gmail.com>
2020-04-01 23:11:07 +0900

    React LifeCycle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
 
-const App =_=> {
-  return (
-    <IterationSample />
-  );
-};
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  };
+
+  render() {
+    return (
+        <div>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycleSample color={this.state.color} />
+        </div>
+    );
+  }
+}
 
 export default App;
